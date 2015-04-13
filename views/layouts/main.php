d480199afc2bce39b3cf445a1817a1391e83c5c9
Gaurav Pant <gauravpantgenext@gmail.com>
2015-04-14 01:52:48 +0530

    OAuth Changes

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 		{
 			 IN.API.Profile("me").fields(["id"]).result(function(result) {
 				var id = result.values[0].id;
-				window.location = '/site/about';	
+				window.location = '/site/login';	
 			 });
 		}
 	</script>
@@ -52,7 +52,8 @@ AppAsset::register($this);
                     ['label' => 'About', 'url' => ['/site/about']],
                     ['label' => 'Contact', 'url' => ['/site/contact']],
                     Yii::$app->user->isGuest ?
-                        ['label' => 'Login', /*'url' => ['/site/login'],*/ 'options'=> ['onclick' => 'IN.User.authorize(LI)']] :
+                        //['label' => 'Login', /*'url' => ['/site/login'],*/ 'options'=> ['onclick' => 'IN.User.authorize(LI)']] :
+						(yii\authclient\widgets\AuthChoice::widget(['baseAuthUrl' => ['site/auth'],'popupMode' => false,]))://
                         ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
                             'url' => ['/site/logout'],
                             'linkOptions' => ['data-method' => 'post']],
@@ -60,7 +61,6 @@ AppAsset::register($this);
             ]);
             NavBar::end();
         ?>
-
         <div class="container">
             <?= Breadcrumbs::widget([
                 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
