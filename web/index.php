4494a5e5c1858a834a4fffdbb9a9e6ca3404a0ee
goodknightrises <gauravpantgenext@gmail.com>
2015-12-14 00:28:53 +0530

    web directory changes

--- web/index.php
+++ web/index.php
@@ -1,8 +1,8 @@
 <?php
-
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
+
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
@@ -10,3 +10,5 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 $config = require(__DIR__ . '/../config/web.php');
 
 (new yii\web\Application($config))->run();
+//$app = new yii\web\Application($config);
+//$app->run();
